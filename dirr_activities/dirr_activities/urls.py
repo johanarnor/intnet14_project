793c32360779e7326c8a2616cc7584ddd996ff64
Jonas <jonas.stendahl@outlook.com>
2014-03-05 09:32:59 +0100

    Nothing  works

--- dirr_activities/dirr_activities/urls.py
+++ dirr_activities/dirr_activities/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('users.urls', namespace='users')),
-    url(r'^main/', include('main.urls', namespace='main')),
+    url(r'^login/', include('users.urls', namespace='users')),
+    url(r'^', include('main.urls', namespace='main')),
 )
