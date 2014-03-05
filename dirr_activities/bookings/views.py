1e910b5a4f67431237a4ba538760e627a19bfe7a
Jonas <jonas.stendahl@outlook.com>
2014-03-05 16:20:52 +0100

    Data added

--- dirr_activities/bookings/views.py
+++ dirr_activities/bookings/views.py
@@ -3,4 +3,7 @@ from bookings.models import Booking
 
 def booking(request, booking_id):
     current_booking = Booking.objects.get(pk=booking_id);
-    return render(request, 'bookings/booking.html', {'current_booking': current_booking})
+    people = current_booking.people_set.all()
+    p = people[0]
+    total = p.senior + p.adult + p.youth + p.child + p.student
+    return render(request, 'bookings/booking.html', {'current_booking': current_booking, 'p': p, 'total': total})
