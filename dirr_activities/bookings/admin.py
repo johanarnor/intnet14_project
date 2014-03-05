9d9dd693f29ee6db7df326c144fbb268a2118e1c
Jonas <jonas.stendahl@outlook.com>
2014-03-05 15:10:04 +0100

    Added models to admin

--- dirr_activities/bookings/admin.py
+++ dirr_activities/bookings/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from bookings.models import Booking
+from bookings.models import Booking, BookingOptions, People
 
 
 admin.site.register(Booking)
+admin.site.register(BookingOptions)
+admin.site.register(People)
